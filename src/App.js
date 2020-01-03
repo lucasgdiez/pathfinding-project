bc86711d17875adf93521af30b8111186f0dbe48
lucasgdiez <lucasgdiez@gmail.com>
2020-01-03 16:07:00 -0300

    Creating files for pathfinder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import PathfindingVisualizer from "./components/PathfindingVisualizer/PathfindingVisualizer";
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <div className='App'>
+      <h1>Pathfinding Visualizer</h1>
+      <PathfindingVisualizer />
+    </div>
+  );
 }
 
 export default App;
