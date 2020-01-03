6035650af31e6c76a36c643bc098c2c55b466d13
lucasgdiez <lucasgdiez@gmail.com>
2020-01-03 17:18:45 -0300

    added props to pathfinding visualizer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <AppWrapper className='App'>
+      <PathfindingVisualizer rows={20} cols={50} />
+    </AppWrapper>
+  );
 }
 
 export default App;
