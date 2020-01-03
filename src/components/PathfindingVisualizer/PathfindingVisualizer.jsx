1f746c2a9e07c1081ea3dcbaebbec7969a2cfbdc
lucasgdiez <lucasgdiez@gmail.com>
2020-01-03 16:35:14 -0300

    mechanism for creating nodes using hooks

--- src/components/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/components/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Node from "./Node/Node";
 
 const PathfindingVisualizer = () => {
+  const [nodes, setNodes] = useState([]);
+
+  useEffect(() => {
+    const nodes = [];
+    for (let row = 0; row < 20; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 50; col++) {
+        currentRow.push([]);
+      }
+      nodes.push(currentRow);
+    }
+    setNodes({ nodes });
+  }, []);
+
   return <Node />;
 };
 
