190ec5a9e21887359c4d738c219d845d9c840faa
lucasgdiez <lucasgdiez@gmail.com>
2020-01-15 15:40:13 -0300

    removed unused code

--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -1,6 +1,11 @@
-const node = {
-  row,
-  col,
-  isVisited,
-  distance
-};
+// Performs Dijkstra's algorithm; returns *all* nodes in the order
+// in which they were visited. Also makes nodes point back to their
+// previous node, effectively allowing us to compute the shortest path
+// by backtracking from the finish node.
+
+export function dijkstra(grid, startNode, finishNode) {
+  //checking edge case scenarios
+  if (!startNode || !finishNode || startNode === finishNode) {
+    return false;
+  }
+}
