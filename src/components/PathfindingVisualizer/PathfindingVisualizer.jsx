904adfcac69da19ae39b53608a8700d95c23b2bc
lucasgdiez <lucasgdiez@gmail.com>
2020-01-30 15:32:48 -0300

    changed values for consts

--- src/components/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/components/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -16,10 +16,10 @@ const GridNode = styled.div`
   flex-direction: row;
 `;
 
-const START_NODE_ROW = 10;
-const START_NODE_COL = 15;
-const FINISH_NODE_ROW = 10;
-const FINISH_NODE_COL = 35;
+const START_NODE_ROW = 2;
+const START_NODE_COL = 3;
+const FINISH_NODE_ROW = 8;
+const FINISH_NODE_COL = 15;
 
 const PathfindingVisualizer = ({ rows, cols }) => {
   const [nodesCount, setNodes] = useState([]);
